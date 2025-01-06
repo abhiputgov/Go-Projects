68d895ec55b770be5b971cd628ebd6863afe6989
Abhishek Puthezhath Govind <abhishek.govind@sap.com>
2025-01-06 23:45:36 +0530

    adding somemodification to read prices from file

--- PriceCalculator/prices/prices.go
+++ PriceCalculator/prices/prices.go
@@ -1,6 +1,10 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type TaxIncludedPriceStruct struct {
 	TaxRate          float64
@@ -8,10 +12,35 @@ type TaxIncludedPriceStruct struct {
 	TaxIncludedPrice map[string]float64
 }
 
+func (jobToRead TaxIncludedPriceStruct) LoadData() []string {
+	file, errOnDataLoad := os.Open("prices.txt")
+	if errOnDataLoad != nil {
+		fmt.Println("Could not open the file.")
+		fmt.Println(errOnDataLoad)
+		return []string{}
+	}
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if scanner.Err() != nil {
+		fmt.Println("Scanning the file failed.")
+		fmt.Println(scanner.Err())
+		file.Close()
+		return []string{}
+	}
+
+	return lines
+
+}
+
 func (job TaxIncludedPriceStruct) Process() {
 	result := make(map[string]float64)
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f at %.2f", price, job.TaxRate*100)] = price * (1 + job.TaxRate)
 	}
 	fmt.Println(result)
 }
