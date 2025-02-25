edd083bb7b553d0a49bb2135ee42679f15b7b7a6
Abhishek Puthezhath Govind <abhishek.govind@sap.com>
2025-02-26 00:12:26 +0530

    modifying the prices calculation by adding calculation via structs

--- PriceCalculator/main.go
+++ PriceCalculator/main.go
@@ -1,19 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go/priceCalculator/prices"
+)
 
 func main() {
-	var prices []float64 = []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	result := make(map[float64][]float64)
 
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
+		prices.NewTaxIncludedPriceJob(taxRate).Process()
 	}
 	fmt.Println(result)
 }
