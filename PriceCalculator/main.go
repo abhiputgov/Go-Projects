0fbca8e1e4714d7d811c4249ee331e79acb86991
Abhishek Puthezhath Govind <abhishek.govind@sap.com>
2025-04-16 14:04:21 +0530

    restarting this module

--- PriceCalculator/main.go
+++ PriceCalculator/main.go
@@ -1,17 +1,1 @@
-package main
 
-import (
-	"fmt"
-	"go/priceCalculator/prices"
-)
-
-func main() {
-	taxRates := []float64{0, 0.07, 0.1, 0.15}
-
-	result := make(map[float64][]float64)
-
-	for _, taxRate := range taxRates {
-		prices.NewTaxIncludedPriceJob(taxRate).Process()
-	}
-	fmt.Println(result)
-}
