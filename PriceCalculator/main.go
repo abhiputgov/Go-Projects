bf0b4dd38a57775587bc2deb75113a60b708ef3b
Abhishek Puthezhath Govind <abhishek.govind@sap.com>
2025-04-16 14:43:12 +0530

    restart changes for price calculator

--- PriceCalculator/main.go
+++ PriceCalculator/main.go
@@ -1,1 +1,20 @@
+package main
 
+import "fmt"
+
+func main() {
+	prices := []float64{10, 20, 30}
+	var taxRates []float64 = []float64{0, 0.07, 0.1, 0.15}
+
+	results := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		var taxIncludedPrice []float64 = make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			taxIncludedPrice[priceIndex] = (1 + taxRate) * price
+		}
+		results[taxRate] = taxIncludedPrice
+	}
+
+	fmt.Println(results)
+}
--- PriceCalculator/prices/prices.go
+++ PriceCalculator/prices/prices.go
@@ -1,1 +1,3 @@
+package prices
 
+func prices() {}
