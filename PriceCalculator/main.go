530cf1a3d24698ffc8789f8a8e908186999aa8d4
Abhishek Puthezhath Govind <abhishek.govind@sap.com>
2025-04-16 15:48:39 +0530

    adding a method part 2

--- PriceCalculator/main.go
+++ PriceCalculator/main.go
@@ -1,20 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"go/priceCalculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	var taxRates []float64 = []float64{0, 0.07, 0.1, 0.15}
-
-	results := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		var taxIncludedPrice []float64 = make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrice[priceIndex] = (1 + taxRate) * price
-		}
-		results[taxRate] = taxIncludedPrice
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
-
-	fmt.Println(results)
 }
